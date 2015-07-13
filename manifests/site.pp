5f817991bce9d284a4b8a755c8a7b4dc8f6cbe01
Lily Moch <lilymoch@gmail.com>
2015-07-13 20:55:26 +0000

    adding cron job to update from repo

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,11 @@
 node ip-10-0-9-201 {
 }
 
-node ip-10-0-9-253 {
+# puppet master node
+node ip-10-0-9-213 {
+    cron { "puppet update":
+        command => "cd /etc/puppet && git pull -q origin master",
+	user    => root,
+	minute  => "*/5",
+    }
 }
