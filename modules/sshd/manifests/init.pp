4f51952ea7982947276164e2141972120ae53fff
root <root@ip-10-0-9-234.us-west-2.compute.internal>
2015-07-15 23:24:13 +0000

    added my (dayv) key

--- modules/sshd/manifests/init.pp
+++ modules/sshd/manifests/init.pp
@@ -24,7 +24,10 @@ class sshd {
 		user => "ubuntu",
 		key  => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCd2UtlFwFhdJtWv6qGvAzrt50yeOLp9hICZmfCQ8cMWXmu7u6bYlMXFtjLhdYOOIc8lROnB8c38DiiOoUitP3BfCXlCCAH6aEA1Je1GvgewuplSm0f/uv5kx0+oa5Dq1KaZ5n6+UUA3HR5eMohQAosr49Y7Mf0h7OasAkZMxCJAuU/h5ytraR21YpN46GDwuTdp9N5mRrROy7OB2TC+hLj4/RCaL+K84j/v1TnDiI5MTS91anC0TBIfCnN3s/MTEh29bu5GYLjtOU2Ti/gBlZylc8CAjr3viqmt0+jTvk11pfx+R8zux7hMLu7qCGfkmwJ0LAB4HxPmPbEzpf7Ki1r",
 	}
-
+	ssh_authorized_key { "dayv-key":
+		user => "ubuntu",
+		key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCDmAoT9TnX9XTOsdv0DwvvnPE+rjH09rjq1pAFfRkKKVkPOB7pUNssgquTUMyiBPE6DZM3u+afKX9qRNbHQEjTF1Sbp/kcpx9yzIfH6oMjlRqXhJog+DS096dr5pOE8/DtuU1x9BGRHJN4g2FIR+YzIvKOnctixDJS9V5Tvhkxzpl66HVhHOTU4i+ALh6Cit50GzA82jcIvVmjweHgAUDXtyXLvRco6k/EGFyFwEQDofgTdHhPqW/s1j2RyMx+PS3lEmUeNUM/eh9fddNZN99HfSPLf1C4wZoNeL5cbNdLAmz/AtQf0NhkVO9j7z0I2eZaoWa+Gau6nOUOB4fjM4vx"
+	}
 	file { "/etc/ssh/sshd_config":
 		source => [
 			"puppet:///modules/sshd/$hostname/sshd_config",
