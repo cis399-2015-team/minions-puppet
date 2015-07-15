086b7b4bde320a0227847ea6c1490c29e3f837ef
Mustafa <malawami91@bitbucket.org>
2015-07-15 16:23:34 -0700

    mufassa keys is updated

--- modules/sshd/manifests/init.pp
+++ modules/sshd/manifests/init.pp
@@ -25,6 +25,11 @@ class sshd {
 		key  => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCd2UtlFwFhdJtWv6qGvAzrt50yeOLp9hICZmfCQ8cMWXmu7u6bYlMXFtjLhdYOOIc8lROnB8c38DiiOoUitP3BfCXlCCAH6aEA1Je1GvgewuplSm0f/uv5kx0+oa5Dq1KaZ5n6+UUA3HR5eMohQAosr49Y7Mf0h7OasAkZMxCJAuU/h5ytraR21YpN46GDwuTdp9N5mRrROy7OB2TC+hLj4/RCaL+K84j/v1TnDiI5MTS91anC0TBIfCnN3s/MTEh29bu5GYLjtOU2Ti/gBlZylc8CAjr3viqmt0+jTvk11pfx+R8zux7hMLu7qCGfkmwJ0LAB4HxPmPbEzpf7Ki1r",
 	}
 
+	ssh_authorized_key { "Mufassa-key":
+		user => "ubuntu",
+		key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCJGcGt69dL6mwivXm1eosJwhA4uNU7VG7bzJdggBFcj4XpGZWXUlJAQE6OMalkvYL91laJM2p/yzF6pFyNZCUsHj53Ef8Fg7hWPpfgtPu1/WOebU7td86T8n0iCuAkYAHtCPyEd4B2eAOwutydRx/0ZNqNBAZnY/oJeGd+3Mxy0vRGtqVQRX1hMl5OjEXZ2i39XppnNMpVCoGsMjcG/6eYvD+KHjip29FfChfjNlVmNaKQQJSPaFQ3wT+UQ+Mobpkt5tI2WcBJIsQ8CB2qKZKbgsHMA8sN9o7vVZY7oz7T01nH3G4xvZtyMfgg0qnTjiICzLsl++n0OtuiZafrQXS9",
+	}
+
 	file { "/etc/ssh/sshd_config":
 		source => [
 			"puppet:///modules/sshd/$hostname/sshd_config",
