389c1efa0fd21d9166152c408f25c4d61d21933f
root <root@ip-10-0-9-201.us-west-2.compute.internal>
2015-07-20 19:22:45 +0000

    added type field to all key resources

--- modules/sshd/manifests/init.pp
+++ modules/sshd/manifests/init.pp
@@ -27,16 +27,19 @@ class sshd {
 	}
 
 	ssh_authorized_key { "lily-test":
+		type => "ssh-rsa",
 		user => "ubuntu",
 		key  => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCMahzx9lkWLOwrEbsikgQji9oNBPXa4JPr/ORDDsBC0FAJg+Zr0a4vNQ3fyEw9GUcyuKeKGey5U8I0GB7xR2xi+R/ksgNOFAKa/jPB483NphzvoHcm3AH9B/BD0kqJ0YvuyTIkHqsWqPeK5TkycKhk+OZ4Pgwkvh",
 	}
 
 	ssh_authorized_key { "dayv-key":
+		type => "ssh-rsa",
 		user => "ubuntu",
 		key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCDmAoT9TnX9XTOsdv0DwvvnPE+rjH09rjq1pAFfRkKKVkPOB7pUNssgquTUMyiBPE6DZM3u+afKX9qRNbHQEjTF1Sbp/kcpx9yzIfH6oMjlRqXhJog+DS096dr5pOE8/DtuU1x9BGRHJN4g2FIR+YzIvKOnctixDJS9V5Tvhkxzpl66HVhHOTU4i+ALh6Cit50GzA82jcIvVmjweHgAUDXtyXLvRco6k/EGFyFwEQDofgTdHhPqW/s1j2RyMx+PS3lEmUeNUM/eh9fddNZN99HfSPLf1C4wZoNeL5cbNdLAmz/AtQf0NhkVO9j7z0I2eZaoWa+Gau6nOUOB4fjM4vx",
 	}
 
 	ssh_authorized_key { "Mufassa-key":
+		type => "ssh-rsa",
 		user => "ubuntu",
 		key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCJGcGt69dL6mwivXm1eosJwhA4uNU7VG7bzJdggBFcj4XpGZWXUlJAQE6OMalkvYL91laJM2p/yzF6pFyNZCUsHj53Ef8Fg7hWPpfgtPu1/WOebU7td86T8n0iCuAkYAHtCPyEd4B2eAOwutydRx/0ZNqNBAZnY/oJeGd+3Mxy0vRGtqVQRX1hMl5OjEXZ2i39XppnNMpVCoGsMjcG/6eYvD+KHjip29FfChfjNlVmNaKQQJSPaFQ3wT+UQ+Mobpkt5tI2WcBJIsQ8CB2qKZKbgsHMA8sN9o7vVZY7oz7T01nH3G4xvZtyMfgg0qnTjiICzLsl++n0OtuiZafrQXS9",
 	}
