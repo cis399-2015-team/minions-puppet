0a1344eeff1c77cf84cffca07bd241034d0aa3b9
mochla <lilymoch@gmail.com>
2015-07-20 23:04:51 +0000

    added the web-server module to Mufassa's and Dave's nodes

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,7 @@
 #Dave's node
 node ip-10-0-9-234 {
 	include sshd
+	include web-server
 }
 
 # Lily's node
@@ -12,11 +13,13 @@ node ip-10-0-9-201 {
 # Mufassa's node
 node ip-10-0-9-51 {
 	include sshd
+	include web-server
 }
 
 # puppet master node
 node ip-10-0-9-213 {
     include sshd
+	include web-server
 
     cron { "puppet update":
         command => "cd /etc/puppet && git pull -q origin master",
